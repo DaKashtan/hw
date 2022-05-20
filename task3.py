53ff1f5cca211cc5a6b1e72cf5e8a9ac63649d65
USER <kashtaan99@gmail.com>
2022-05-20 03:09:57 +0300

    Обработка исключения в task3

--- task3.py
+++ task3.py
@@ -1,6 +1,9 @@
 import re
-
+import os.path
 def pattern(fprimer, rprimer):
+    if not fprimer or rprimer:
+        raise TypeError("Не задана последовательность праймера!")
+
     fprimer, rprimer = fprimer.upper(), rprimer.upper()
     fdict = {'A': 'A', 'T': 'T', 'C': 'C', 'G': 'G', 'B': '[CGT]', 'D': '[AGT]', 'H': '[ACT]', 'K': '[GT]',
                  'M': '[AC]', 'N': '[ACGT]', 'R': '[GA]', 'S': '[GC]', 'V': '[ACG]', 'W': '[AT]', 'Y': '[CT]'}
@@ -13,15 +16,22 @@ def pattern(fprimer, rprimer):
     for j in rprimer:
         pattern += rdict[j]
     return pattern
-
-with open(input('Введите имя файла в формате fasta: ') as seq_file:
-    name_sequence = []
-    sequence = []
-    for l in seq_file:
-        if '>' in l:
-            name_sequence.append(l.strip())
-        else:
-            sequence.append(l.strip())
+file=input('Введите имя файла в формате fasta: ')
+if not file:
+    raise TypeError("Не указано имя файла")
+elif not os.path.exists(file):
+    raise FileNotFoundError(f'Файл {file} не найден ')
+elif '.fa' not in file:
+    raise TypeError('Файл должен быть в формате *.fasta')
+else:
+    with open(file) as seq_file:
+        name_sequence = []
+        sequence = []
+        for l in seq_file:
+            if '>' in l:
+                name_sequence.append(l.strip())
+            else:
+                sequence.append(l.strip())
 fprimer = input('Прямой праймер: ')
 rprimer = input('Обратный праймер: ')
 patt = pattern(fprimer, rprimer)
